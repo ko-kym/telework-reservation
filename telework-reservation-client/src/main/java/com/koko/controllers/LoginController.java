9136b370686f6d062e9b7b7a8de27ff42d7a0f5e
ko-kym <koutaroukym000@gmail.com>
2024-07-28 19:00:22 +0900

    refactor: URLクラスの非推奨対応

--- telework-reservation-client/src/main/java/com/koko/controllers/LoginController.java
+++ telework-reservation-client/src/main/java/com/koko/controllers/LoginController.java
@@ -4,6 +4,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 
 import jakarta.servlet.RequestDispatcher;
@@ -25,16 +28,15 @@ public class LoginController extends HttpServlet {
 
         response.setContentType("text/html");
         if (apiResponse.equals("login success")) {
-            
+
             RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/view/calendar.jsp");
             dispatcher.forward(request, response);
-            
+
         } else {
             request.setAttribute("errorMessage", "メールアドレスかパスワードが違います");
             RequestDispatcher dispatcher = request.getRequestDispatcher("/index.jsp");
             dispatcher.forward(request, response);
         }
-
 
     }
 
@@ -45,17 +47,27 @@ public class LoginController extends HttpServlet {
 
     private String callLogin(String email, String password) throws IOException {
         String endPoint = "http://localhost:8080/telework-reservation-api-1.0/login";
-        URL url = new URL(endPoint + "?email=" + email + "&password=" + password);
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        connection.setRequestMethod("GET");
+        URL url;
+        try {
+            url = new URI(endPoint.concat("?email=").concat(email).concat("&password=").concat(password)).toURL();
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
 
-        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-        String inputLine;
-        StringBuffer response = new StringBuffer();
-        while ((inputLine = in.readLine()) != null) {
-            response.append(inputLine);
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+
+            in.close();
+            return response.toString();
+
+        } catch (MalformedURLException | URISyntaxException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            return null;
         }
-        in.close();
-        return response.toString();
+
     }
 }
