d2bb0a0bb4aa581b15f5c3ee3fd2a379b9b96faf
ko-kym <koutaroukym000@gmail.com>
2024-07-28 18:44:51 +0900

    feat: プロジェクト2つを同時にデプロイできるように変更

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 # 環境変数の設定
-PROJECT_DIR=$(pwd)
-WAR_FILE="telework-reservation-1.0.war"
+API_DIR="./telework-reservation-api"
+CLIENT_DIR="./telework-reservation-client"
+API_WAR="telework-reservation-api-1.0.war"
+CLIENT_WAR="telework-reservation-client-1.0.war"
 
 # UTF-8エンコーディングに設定
 export LANG=en_US.UTF-8
@@ -11,14 +13,15 @@ export LANG=en_US.UTF-8
 $TOMCAT_HOME/bin/catalina.sh stop
 
 # MavenでWARファイルを作成
-mvn clean package
+mvn -f $API_DIR clean package 
+mvn -f $CLIENT_DIR clean package 
 
 if [ $? -eq 0 ]; then
     echo "WAR file created successfully."
-    cp $PROJECT_DIR/target/$WAR_FILE $TOMCAT_HOME/webapps/
+    cp $API_DIR/target/$API_WAR $TOMCAT_HOME/webapps/
+    cp $CLIENT_DIR/target/$CLIENT_WAR $TOMCAT_HOME/webapps/
     echo "WAR file copied to Tomcat webapps directory."
     $TOMCAT_HOME/bin/catalina.sh start
-    # echo "Tomcat started."
 else
     echo "Failed to create WAR file."
 fi
