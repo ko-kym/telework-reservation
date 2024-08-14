e1e0030e92a7705df77a14f95cd53c72ef623077
ko-kym <koutaroukym000@gmail.com>
2024-08-14 23:43:13 +0900

    feat: clientのSpringフレームワーク移行

--- telework-reservation-client/src/main/java/com/koko/controllers/LogoutController.java
+++ telework-reservation-client/src/main/java/com/koko/controllers/LogoutController.java
@@ -2,19 +2,18 @@ package com.koko.controllers;
 
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import org.springframework.web.HttpRequestHandler;
+
 import java.io.IOException;
 
-@WebServlet("/call-logout")
-public class LogoutController extends HttpServlet {
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+public class LogoutController implements HttpRequestHandler {
 
+    @Override
+    public void handleRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
         RequestDispatcher dispatcher = request.getRequestDispatcher("/index.jsp");
         dispatcher.forward(request, response);
     }
